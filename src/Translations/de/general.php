77e883c7052edc043f303e8f6253220d1c779ea0
24aitor <contact@aitorriba.com>
2017-04-15 18:52:32 +0200

    Added Translations

---
+++ src/Translations/de/general.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Payments Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'payments' => 'Zahlungen',
+    'payments_desc' => 'Sie können Ihr Einkommen ansehen und verwalten und detaillierte Statistiken überprüfen.',
+    'pending_balance' => 'Ausstehendes Gleichgewicht',
+    'available_balance' => 'Verfügbares Guthaben',
+    'home' => 'Zuhause',
+    'charges' => 'Gebühren',
+    'amount' => 'Menge',
+    'status' => 'Status',
+    'date' => 'Datum',
+    'paid' => 'Bezahlt',
+    'not_paid' => 'Nicht bezahlt',
+    'no_keys_set' => 'Sie müssen beide Stipe-Tasten einrichten, um diese Seite zu sehen.',
+    'customers' => 'Kunden',
+    'email' => 'Email',
+    'description' => 'Beschreibung',
+    'live_mode' => 'Live-Modus',
+    'test_mode' => 'Testmodus',
+    'stripe_key' => 'Stripe Publishable Key',
+    'stripe_key_ph' => 'Geben Sie Ihren Streifen veröffentlichbaren Schlüssel ein',
+    'stripe_key_hp' => 'https://dashboard.stripe.com/account/apikeys',
+    'stripe_secret' => 'Stripe Secret Key',
+    'stripe_secret_ph' => 'Geben Sie Ihren Stripe-Geheimschlüssel ein',
+    'stripe_secret_hp' => 'https://dashboard.stripe.com/account/apikeys',
+    'save' => 'Sende Zahlungen Einstellungen',
+    'updated_settings' => 'Die Zahlungen wurden aktualisiert',
+    'unauthorized_action' => 'Unbefugte Handlung',
+    'unauthorized_desc' => 'Sie können die Zahlungseinstellungen nicht aktualisieren.',
+    'contact_webmaster' => 'Wenn du denkst, du solltest es in der Lage sein, es zu aktualisieren, bitte wenden Sie sich an webmaster.',
+];
---
+++ src/Translations/fr/general.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Payments Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'payments' => 'Paiements',
+    'payments_desc' => 'Vous pouvez afficher et gérer vos revenus et vérifier les statistiques détaillées.',
+    'pending_balance' => 'Balance en attente',
+    'available_balance' => 'Solde disponible',
+    'home' => 'Accueil',
+    'charges' => 'Des charges',
+    'amount' => 'Montant',
+    'status' => 'Statut',
+    'date' => 'Rendez-vous amoureux',
+    'paid' => 'Payé',
+    'not_paid' => 'Impayé',
+    'no_keys_set' => 'Vous devez configurer les deux touches stipe pour voir cette page.',
+    'customers' => 'Les clients',
+    'email' => 'Email',
+    'description' => 'La description',
+    'live_mode' => 'Mode en direct',
+    'test_mode' => "Mode d'essai",
+    'stripe_key' => 'Clé lisible à rayures',
+    'stripe_key_ph' => 'Entrez votre clé publiable',
+    'stripe_key_hp' => 'https://dashboard.stripe.com/account/apikeys',
+    'stripe_secret' => 'Strike Secret Key',
+    'stripe_secret_ph' => 'Entrez votre clé secrète',
+    'stripe_secret_hp' => 'https://dashboard.stripe.com/account/apikeys',
+    'save' => 'Enregistrer les paramètres de paiement',
+    'updated_settings' => 'Les paramètres de paiement ont été mis à jour',
+    'unauthorized_action' => 'Action non autorisée',
+    'unauthorized_desc' => "Vous n'êtes pas autorisé à mettre à jour les paramètres de paiement.",
+    'contact_webmaster' => 'Si vous pensez pouvoir le mettre à jour, contactez le webmaster.',
+];
